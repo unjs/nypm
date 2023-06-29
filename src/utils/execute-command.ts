6c6d5828d4f487b45dbc048c26a7706d7b2c9b15
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-06-29 21:26:42 +0200

    fix: revert to workaround for `corepack npm` on ubuntu

--- src/utils/execute-command.ts
+++ src/utils/execute-command.ts
@@ -8,7 +8,12 @@ export async function executeCommand(
   const { execa } = await import("execa");
   const { resolve } = await import("pathe");
 
-  await execa("corepack", [command, ...args], {
+  // workaround issue with segmentation fault when using corepack with npm
+  // on ubuntu-latest
+  const execaArgs: [string, string[]] =
+    command === "npm" ? [command, args] : ["corepack", [command, ...args]];
+
+  await execa(execaArgs[0], execaArgs[1], {
     cwd: resolve(options.cwd || process.cwd()),
     stdio: options.silent ? "ignore" : "inherit",
   });
