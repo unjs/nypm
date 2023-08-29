6bd71aacf9f43a7a38852968c3c4965c01b2a160
Pooya Parsa <pooya@pi0.io>
2023-08-29 23:24:32 +0200

    split tests

---
+++ test/_shared.ts
@@ -0,0 +1,65 @@
+import { fileURLToPath } from "node:url";
+import type { PackageManagerName } from "../src";
+
+export type Fixture = {
+  name: string;
+  dir: string;
+  packageManager: PackageManagerName;
+  majorVersion?: string;
+  workspace: boolean;
+};
+
+export const fixtures = (
+  [
+    {
+      name: "bun",
+      packageManager: "bun",
+    },
+    {
+      name: "bun-workspace",
+      packageManager: "bun",
+    },
+    {
+      name: "npm",
+      packageManager: "npm",
+    },
+    {
+      name: "npm-workspace",
+      packageManager: "npm",
+    },
+    {
+      name: "pnpm",
+      packageManager: "pnpm",
+    },
+    {
+      name: "pnpm-workspace",
+      packageManager: "pnpm",
+    },
+    {
+      name: "yarn-classic",
+      packageManager: "yarn",
+    },
+    {
+      name: "yarn-classic-workspace",
+      packageManager: "yarn",
+    },
+    {
+      name: "yarn-berry",
+      packageManager: "yarn",
+      majorVersion: "3",
+    },
+    {
+      name: "yarn-berry-workspace",
+      packageManager: "yarn",
+      majorVersion: "3",
+    },
+  ] satisfies Partial<Fixture>[]
+).map((fixture) => ({
+  ...fixture,
+  dir: resolveFixtureDirectory(fixture.name),
+  workspace: fixture.name.includes("workspace"),
+}));
+
+export function resolveFixtureDirectory(name: string) {
+  return fileURLToPath(new URL(`fixtures/${name}`, import.meta.url));
+}
---
+++ test/api-workspace.test.ts
@@ -0,0 +1,67 @@
+import { expect, it, describe, vi } from "vitest";
+import { isWindows } from "std-env";
+import { addDependency, removeDependency } from "../src";
+import { fixtures } from "./_shared";
+
+describe("api", () => {
+  for (const fixture of fixtures.filter((f) => f.workspace)) {
+    // bun is not yet supported on Windows
+    if (isWindows && fixture.packageManager === "bun") {
+      continue;
+    }
+    describe(fixture.name, () => {
+      it("adds dependency to the workspace root", async () => {
+        const addDependencySpy = vi.fn(addDependency);
+        const executeAddDependencySpy = () =>
+          addDependencySpy("pathe", {
+            cwd: fixture.dir,
+            silent: !process.env.DEBUG,
+            workspace: true,
+          });
+        await executeAddDependencySpy();
+        expect(addDependencySpy).toHaveReturned();
+      }, 30_000);
+
+      it("removes dependency from workspace root", async () => {
+        const removeDependencySpy = vi.fn(removeDependency);
+        const executeRemoveDependencySpy = () =>
+          removeDependencySpy("pathe", {
+            cwd: fixture.dir,
+            silent: !process.env.DEBUG,
+            workspace: true,
+          });
+        await executeRemoveDependencySpy();
+        expect(removeDependencySpy).toHaveReturned();
+      }, 30_000);
+
+      const workspaceRef =
+        fixture.name === "yarn-classic-workspace"
+          ? "./packages/workspace-a"
+          : "workspace-a";
+
+      it("adds dependency to workspace package", async () => {
+        const addDependencySpy = vi.fn(addDependency);
+        const executeAddDependencySpy = () =>
+          addDependencySpy("ufo", {
+            cwd: fixture.dir,
+            silent: !process.env.DEBUG,
+            workspace: workspaceRef,
+          });
+        await executeAddDependencySpy();
+        expect(addDependencySpy).toHaveReturned();
+      }, 30_000);
+
+      it("removes dependency from workspace package", async () => {
+        const removeDependencySpy = vi.fn(removeDependency);
+        const executeRemoveDependencySpy = () =>
+          removeDependencySpy("ufo", {
+            cwd: fixture.dir,
+            silent: !process.env.DEBUG,
+            workspace: workspaceRef,
+          });
+        await executeRemoveDependencySpy();
+        expect(removeDependencySpy).toHaveReturned();
+      }, 30_000);
+    });
+  }
+});
