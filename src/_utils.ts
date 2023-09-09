25089325449c10c90861c5d7a81c3454a366b0bd
Pooya Parsa <pooya@pi0.io>
2023-09-09 13:02:14 +0200

    fix: fallback to local package manager when corepack not installed

--- src/_utils.ts
+++ src/_utils.ts
@@ -1,5 +1,5 @@
 import { createRequire } from "node:module";
-import { normalize } from "pathe";
+import { normalize, resolve } from "pathe";
 import { withTrailingSlash } from "ufo";
 import type { OperationOptions } from "./types";
 import type { DetectPackageManagerOptions } from "./package-manager";
@@ -24,21 +24,41 @@ export async function findup<T>(
   }
 }
 
+function cached<T>(fn: () => Promise<T>): () => T | Promise<T> {
+  let p: T | Promise<T> | undefined;
+  return () => {
+    if (p === undefined) {
+      p = Promise.resolve(fn()).then((v) => {
+        p = v;
+        return v;
+      });
+    }
+    return p;
+  };
+}
+
+const importExeca = cached(() => import("execa").then((r) => r.execa));
+const hasCorepack = cached(async () => {
+  try {
+    const execa = await importExeca();
+    await execa("corepack", ["--version"]);
+    return true;
+  } catch {
+    return false;
+  }
+});
+
 export async function executeCommand(
   command: string,
   args: string[],
   options: Pick<OperationOptions, "cwd" | "silent"> = {},
 ): Promise<void> {
-  const { execa } = await import("execa");
-  const { resolve } = await import("pathe");
-
-  // work around issue with segmentation fault when using corepack with npm
-  // on ubuntu-latest
   const execaArgs: [string, string[]] =
-    command === "npm" || command === "bun"
+    command === "npm" || command === "bun" || !(await hasCorepack())
       ? [command, args]
       : ["corepack", [command, ...args]];
 
+  const execa = await importExeca();
   await execa(execaArgs[0], execaArgs[1], {
     cwd: resolve(options.cwd || process.cwd()),
     stdio: options.silent ? "pipe" : "inherit",
